eb8e96bd21b7919f1c99808d6fd81e30a2deec04
ji-one <ds020228@naver.com>
2023-01-26 01:19:27 +0900

    feat: 회원 도메인 개발

---
+++ src/main/java/hello/core/member/MemoryMemberRepository.java
@@ -0,0 +1,19 @@
+package hello.core.member;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MemoryMemberRepository implements MemberRepository {
+
+    private static Map<Long, Member> store = new HashMap<>(); // 실무에서는 ConcurrnetHashMap
+
+    @Override
+    public void save(Member member) {
+        store.put(member.getId(), member);
+    }
+
+    @Override
+    public Member findById(Long memeberId) {
+        return store.get(memeberId);
+    }
+}
