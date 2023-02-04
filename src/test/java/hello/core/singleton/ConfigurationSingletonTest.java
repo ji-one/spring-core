c4655066cc7a02ce5f69281fbe39562525e765ee
ji-one <rlawldnjs8@gmail.com>
2023-02-04 22:30:46 +0900

    test: @Configuration Test - @Bean만 사용해도 스프링 빈으로 등록되지만, 싱글톤을 보장하지 않는다. - 스프링 설정 정보는 항상 @Configuration을 사용하자.

--- src/test/java/hello/core/singleton/ConfigurationSingletonTest.java
+++ src/test/java/hello/core/singleton/ConfigurationSingletonTest.java
@@ -1,6 +1,6 @@
 package hello.core.singleton;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 import hello.core.AppConfig;
 import hello.core.member.MemberRepository;
@@ -13,7 +13,7 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 public class ConfigurationSingletonTest {
 
     @Test
-    void configurationTest(){
+    void configurationTest() {
         ApplicationContext ac = new AnnotationConfigApplicationContext(
             AppConfig.class);
 
@@ -30,5 +30,18 @@ public class ConfigurationSingletonTest {
         // 모두 같은 인스턴스를 참고하고 있다.
         assertThat(memberService.getMemberRepository()).isSameAs(memberRepository);
         assertThat(orderService.getMemberRepository()).isSameAs(memberRepository);
+    }
+
+    @Test
+    void configurationDeep() {
+        ApplicationContext ac = new AnnotationConfigApplicationContext(
+            AppConfig.class);
+        AppConfig bean = ac.getBean(AppConfig.class);
+
+        // 순수한 클래스라면 hello.core.AppConfig 출력되어야 한다.
+        // 결과: hello.core.AppConfig$$EnhancerBySpringCGLIB$$79b7202a
+        // 스프링이 내가 만든 클래스가 아니라 스프링이 CGLIB라는 바이트 코드 조작 라이브러리를 사용해서 AppConfig 클래스를 상속받은 임의의 다른 클래스를 만들고
+        // 그 클래스를 스프링 빈으로 등록함.
+        System.out.println("bean = " + bean.getClass());
     }
 }
