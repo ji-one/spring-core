49c22c9b56e0f86bcbe75e5c7c2f0749567fbb8f
ji-one <rlawldnjs8@gmail.com>
2023-02-04 21:43:43 +0900

    test: 싱글톤 깨지는지 테스트 - @Bean memberService -> new MemoryMemberRepository() - @Bean orderService -> new MemoryMemberRepository() - 각각 다른 2개의 MemoryMemberRepository가 생성되면서 싱글톤 깨지는게 아닐까?

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -14,18 +14,35 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class AppConfig {
 
+    // @Bean memberService -> new MemoryMemberRepository()
+    // @Bean orderService -> new MemoryMemberRepository()
+    // 각각 다른 2개의 MemoryMemberRepository가 생성되면서 싱글톤 깨지는게 아닐까?
+
+    // call AppConfig.memberService
+    // call AppConfig.memberRepository
+    // call AppConfig.memberRepository
+    // call AppConfig.orderService
+    // call AppConfig.memberRepository
+
+    // call AppConfig.memberService
+    // call AppConfig.memberRepository
+    // call AppConfig.orderService
+
     @Bean // spring container 에 등록
     public MemberService memberService() {
+        System.out.println("call AppConfig.memberService");
         return new MemberServiceImpl(memberRepository()); // 생성자 주입
     }
 
     @Bean
     public MemberRepository memberRepository() {
+        System.out.println("call AppConfig.memberRepository");
         return new MemoryMemberRepository();
     }
 
     @Bean
     public OrderService orderService() {
+        System.out.println("call AppConfig.orderService");
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -21,4 +21,9 @@ public class OrderServiceImpl implements OrderService { // 인터페이스에만
 
         return new Order(memberId, itemName, itemPrice, discountPrice);
     }
+
+    // 테스트 용도
+    public MemberRepository getMemberRepository() {
+        return memberRepository;
+    }
 }
