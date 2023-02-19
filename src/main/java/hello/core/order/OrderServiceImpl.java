a797d85a56e65cf40c4225e66a9309738a1bb30e
ji-one <rlawldnjs8@gmail.com>
2023-02-19 20:21:40 +0900

    feat: 다양한 의존관계 주입 방법 - 생성자 주입 - 수정자 주입(setter 주입) - 필드 주입 - 일반 메서드 주입

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -9,12 +9,24 @@ import org.springframework.stereotype.Component;
 @Component
 public class OrderServiceImpl implements OrderService { // 인터페이스에만 의존하고 있다. DIP 충족. 실행에만 집중하면 된다.
 
-    private final MemberRepository memberRepository;
-    private final DiscountPolicy discountPolicy;
+    private MemberRepository memberRepository;
+    private DiscountPolicy discountPolicy;
 
     @Autowired
     public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
         this.memberRepository = memberRepository;
+        this.discountPolicy = discountPolicy;
+    }
+
+    // 일반 메서드 주입
+    @Autowired
+    public void init(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+        this.memberRepository = memberRepository;
+        this.discountPolicy = discountPolicy;
+    }
+
+    @Autowired
+    public void setDiscountPolicy(DiscountPolicy discountPolicy) {
         this.discountPolicy = discountPolicy;
     }
 
@@ -26,8 +38,15 @@ public class OrderServiceImpl implements OrderService { // 인터페이스에만
         return new Order(memberId, itemName, itemPrice, discountPrice);
     }
 
-    // 테스트 용도
     public MemberRepository getMemberRepository() {
         return memberRepository;
+    }
+    // 테스트 용도
+
+    // setter라 불리는 필드의 값을 변경하는 수정자 메서드를 통해서 의존관계를 주입하는 방법 - 선택, 변경 가능성이 있는 의존관계에 사용
+    @Autowired(required = false) // 주입할 대상이 없어도 동작하게 하려면 @Autowired(required = false)로 지정
+    public void setMemberRepository(
+        MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
     }
 }
