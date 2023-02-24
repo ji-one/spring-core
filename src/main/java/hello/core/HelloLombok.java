27ccacc23a9ca004b1868c7a3775ab0f190f06ea
ji-one <rlawldnjs8@gmail.com>
2023-02-25 02:14:50 +0900

    feat: lombok 적용

---
+++ src/main/java/hello/core/HelloLombok.java
@@ -0,0 +1,27 @@
+package hello.core;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class HelloLombok {
+
+    private String name;
+    private int age;
+
+    public static void main(String[] args) {
+        HelloLombok helloLombok = new HelloLombok();
+        helloLombok.setName("foo");
+        helloLombok.setAge(10);
+
+        String name = helloLombok.getName();
+        int age = helloLombok.getAge();
+        System.out.println("name = " + name);
+        System.out.println("age = " + age);
+        
+    }
+
+}
---
+++ src/test/java/hello/core/order/OrderServiceImplTest.java
@@ -0,0 +1,24 @@
+package hello.core.order;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+import hello.core.discount.FixDiscountPolicy;
+import hello.core.member.Grade;
+import hello.core.member.Member;
+import hello.core.member.MemoryMemberRepository;
+import org.junit.jupiter.api.Test;
+
+class OrderServiceImplTest {
+
+    @Test
+    void createOrder(){
+        MemoryMemberRepository memberRepository = new MemoryMemberRepository();
+        memberRepository.save(new Member(1L, "name", Grade.VIP));
+
+        OrderServiceImpl orderService = new OrderServiceImpl(new MemoryMemberRepository(),
+            new FixDiscountPolicy());
+        Order order = orderService.createOrder(1L, "itemA", 10000);
+        assertThat(order.getDiscountPrice()).isEqualTo(1000);
+    }
+}
